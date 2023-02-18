cf948e1ae5c10261717d3775131a7b618820cb07
Farhod <mr.hokimiyon@gmail.com>
2023-02-18 23:32:32 +0300

    testing

--- src/modules/sender.py
+++ src/modules/sender.py
@@ -5,18 +5,23 @@ Sender main page
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ContextTypes
 
-from ..utils.util import build_menu
-
 SENDER_PAGE = 3
 
 
 async def SenderHandler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    button_list = [
-        InlineKeyboardButton("My offers", callback_data=str(1)),
-        InlineKeyboardButton("Approves", callback_data=str(2)),
-        InlineKeyboardButton("Create offer", callback_data=str(3)),
-        InlineKeyboardButton("Cancel offer", callback_data=str(4)),
+    query = update.callback_query
+    await query.answer()
+    keyboard = [
+        [
+            InlineKeyboardButton("My offers", callback_data=str(1)),
+            InlineKeyboardButton("Approves", callback_data=str(2)),
+            InlineKeyboardButton("Create offer", callback_data=str(3)),
+            InlineKeyboardButton("Cancel offer", callback_data=str(4)),
+        ]
     ]
-    reply_markup = InlineKeyboardMarkup(build_menu(button_list, n_cols=4))
-    await update.message.reply_text('Choose what you want to do', reply_markup=reply_markup)
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await query.edit_message_text(
+        text="Choose what you want to do", reply_markup=reply_markup
+    )
+
     return SENDER_PAGE
