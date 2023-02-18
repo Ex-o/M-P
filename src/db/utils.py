b48553a5c0a5533f5562d80fe870b29624a17b4e
Farhod <mr.hokimiyon@gmail.com>
2023-02-19 01:11:05 +0300

    testing

--- src/db/utils.py
+++ src/db/utils.py
@@ -4,11 +4,11 @@ from .db_context import DBConn as db
 def register_user(telegram_id):
     with db as db_ctx:
         db_ctx.execute(
-            f"INSERT INTO users (id) VALUES({telegram_id})")
+            f'INSERT INTO users (id) VALUES("{telegram_id}")')
 
 
 def set_offer(loc_destination, loc_source, cost):
     with db() as db_ctx:
         db_ctx.execute(
-            "INSERT INTO offers (loc_destination, loc_source, cost) "
-            f"VALUES({loc_destination}, {loc_source}, {cost})")
+            'INSERT INTO offers (loc_destination, loc_source, cost) '
+            f'VALUES ("{loc_destination}", "{loc_source}", "{cost})"')
