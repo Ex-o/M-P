2a61856f5130edeacc4effe66a4a1c49c0161f5b
Khaled Mohamed <Ex-o@users.noreply.github.com>
2023-02-20 14:22:11 +0300

    testing

--- src/data/states.py
+++ src/data/states.py
@@ -1,6 +1,6 @@
 from telegram.ext import CallbackQueryHandler, MessageHandler, filters
 
-from src.modules.cancel_offer import cancel_offer_handler, cancel_confirm_handler
+from src.modules.cancel_offer import cancel_offer_handler, cancel_confirm_handler, destroy_offer_handler
 from src.modules.create_offer import create_offer_handler, meeting_place_handler, food_place_handler, payment_handler
 from src.modules.end import end_handler
 from src.data.pages import *
@@ -37,7 +37,7 @@ CANCEL_OFFER_STATE = {
         CallbackQueryHandler(cancel_confirm_handler, pattern="^(0|[1-9][0-9]*)$")
     ],
     CANCEL_CONFIRM_PAGE : [
-        CallbackQueryHandler(end_handler, pattern="^" + str(1) + "$"),
+        CallbackQueryHandler(destroy_offer_handler, pattern="^" + str(1) + "$"),
         CallbackQueryHandler(end_handler, pattern="^" + str(2) + "$"),
     ]
 }
