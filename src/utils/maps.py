6fa1a75ed5a79929f0d0b0844679a4a5d7e12a68
Khaled Mohamed <Ex-o@users.noreply.github.com>
2023-03-03 17:09:43 +0300

    fixing lon/lat

--- src/utils/maps.py
+++ src/utils/maps.py
@@ -6,12 +6,12 @@ from playwright.async_api import async_playwright
 
 
 async def get_ll_from_yandex_url(url):
-    with async_playwright() as p:
-        for browser_type in [p.chromium]:
-            browser = await browser_type.launch()
-            page = await browser.new_page()
-            await page.goto(url)
-            bs = BeautifulSoup(await page.content(), features="html.parser")
-            test = bs.find("script", {"class": "state-view"}).next
-            js = json.loads(test)
-            return js['map']['location']['center']
+    p = await async_playwright().start()
+    for browser_type in [p.chromium]:
+        browser = await browser_type.launch()
+        page = await browser.new_page()
+        await page.goto(url)
+        bs = BeautifulSoup(await page.content(), features="html.parser")
+        test = bs.find("script", {"class": "state-view"}).next
+        js = json.loads(test)
+        return js['map']['location']['center']
