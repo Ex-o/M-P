71231f93dfe75db0c52793689911057b13ecad8d
Khaled Mohamed <Ex-o@users.noreply.github.com>
2023-03-07 13:52:43 +0300

    testing webserver

--- bot.py
+++ bot.py
@@ -147,7 +147,7 @@ async def main() -> None:
 
     async def custom_updates(request: Request) -> PlainTextResponse:
 
-        logger.info(request)
+        logger.debug(request)
         return PlainTextResponse("Thank you for the submission! It's being forwarded.")
         """
         Handle incoming webhook updates by also putting them into the `update_queue` if
