a4786db7be5095f93c7f8e6aa39a8beaf4c33124
Khaled Mohamed <Ex-o@users.noreply.github.com>
2023-03-07 13:32:53 +0300

    testing webserver

--- bot.py
+++ bot.py
@@ -14,6 +14,7 @@ bot.
 """
 import sys
 import asyncio
+from dataclasses import dataclass
 from http import HTTPStatus
 
 import uvicorn
@@ -22,8 +23,8 @@ from starlette.requests import Request
 from starlette.responses import PlainTextResponse, Response
 from starlette.routing import Route
 
-from telegram import __version__ as TG_VER
-
+from telegram import __version__ as TG_VER, Update
+from telegram.constants import ParseMode
 
 try:
     from telegram import __version_info__
@@ -41,7 +42,8 @@ if __version_info__ < (20, 0, 0, "alpha", 1):
 import os
 import logging
 
-from telegram.ext import Application, CommandHandler, ContextTypes, ConversationHandler
+from telegram.ext import Application, CommandHandler, ContextTypes, ConversationHandler, CallbackContext, ExtBot, \
+    TypeHandler
 
 from src.modules.start import start_handler
 from src.modules.help import help_handler
@@ -57,14 +59,63 @@ logger = logging.getLogger(__name__)
 TOKEN = os.environ['TG_TOKEN']
 PORT = int(os.environ.get('PORT', 5000))
 
+@dataclass
+class WebhookUpdate:
+    """Simple dataclass to wrap a custom update type"""
+
+    user_id: int
+    payload: str
+
+
+class CustomContext(CallbackContext[ExtBot, dict, dict, dict]):
+    """
+    Custom CallbackContext class that makes `user_data` available for updates of type
+    `WebhookUpdate`.
+    """
+
+    @classmethod
+    def from_update(
+        cls,
+        update: object,
+        application: "Application",
+    ) -> "CustomContext":
+        if isinstance(update, WebhookUpdate):
+            return cls(application=application, user_id=update.user_id)
+        return super().from_update(update, application)
+
+
+async def webhook_update(update: WebhookUpdate, context: CustomContext) -> None:
+    """Callback that handles the custom updates."""
+    chat_member = await context.bot.get_chat_member(chat_id=update.user_id, user_id=update.user_id)
+    payloads = context.user_data.setdefault("payloads", [])
+    payloads.append(update.payload)
+    combined_payloads = "</code>\n• <code>".join(payloads)
+    text = (
+        f"The user {chat_member.user.mention_html()} has sent a new payload. "
+        f"So far they have sent the following payloads: \n\n• <code>{combined_payloads}</code>"
+    )
+    await context.bot.send_message(
+        chat_id=context.bot_data["admin_chat_id"], text=text, parse_mode=ParseMode.HTML
+    )
+
 
 async def main() -> None:
-    """Start the bot."""
-    # Create the Application and pass it your bot's token.
+    """Set up the application and a custom webserver."""
+    url = "https://boiling-ravine-21139.herokuapp.com"
+    admin_chat_id = 123456
+    port = PORT
 
-    application = Application.builder().token(TOKEN).build()
+    context_types = ContextTypes(context=CustomContext)
+    # Here we set updater to None because we want our custom webhook server to handle the updates
+    # and hence we don't need an Updater instance
+    application = (
+        Application.builder().token("TOKEN").updater(None).context_types(context_types).build()
+    )
+    # save the values in `bot_data` such that we may easily access them in the callbacks
+    application.bot_data["url"] = url
+    application.bot_data["admin_chat_id"] = admin_chat_id
 
-    # on different commands - answer in Telegram
+    # register handlers
     application.add_handler(CommandHandler("help", help_handler))
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler("start", start_handler)],
@@ -81,34 +132,69 @@ async def main() -> None:
         fallbacks=[CommandHandler("start", start_handler)],
     )
     application.add_handler(conv_handler)
-    application.run_webhook(listen="0.0.0.0",
-                            port=int(PORT),
-                            url_path=TOKEN,
-                            webhook_url='https://boiling-ravine-21139.herokuapp.com/' + TOKEN)
+    application.add_handler(TypeHandler(type=WebhookUpdate, callback=webhook_update))
+
+    # Pass webhook settings to telegram
+    await application.bot.set_webhook(url=f"{url}/telegram")
+
+    # Set up webserver
+    async def telegram(request: Request) -> Response:
+        """Handle incoming Telegram updates by putting them into the `update_queue`"""
+        await application.update_queue.put(
+            Update.de_json(data=await request.json(), bot=application.bot)
+        )
+        return Response()
 
     async def custom_updates(request: Request) -> PlainTextResponse:
+
+        logger.info(request)
+        return None
         """
         Handle incoming webhook updates by also putting them into the `update_queue` if
         the required parameters were passed correctly.
         """
-        logger.info(request)
+        try:
+            user_id = int(request.query_params["user_id"])
+            payload = request.query_params["payload"]
+        except KeyError:
+            return PlainTextResponse(
+                status_code=HTTPStatus.BAD_REQUEST,
+                content="Please pass both `user_id` and `payload` as query parameters.",
+            )
+        except ValueError:
+            return PlainTextResponse(
+                status_code=HTTPStatus.BAD_REQUEST,
+                content="The `user_id` must be a string!",
+            )
+
+        await application.update_queue.put(WebhookUpdate(user_id=user_id, payload=payload))
+        return PlainTextResponse("Thank you for the submission! It's being forwarded.")
+
+    async def health(_: Request) -> PlainTextResponse:
+        """For the health endpoint, reply with a simple plain text message."""
+        return PlainTextResponse(content="The bot is still running fine :)")
 
     starlette_app = Starlette(
         routes=[
-            Route("/bgk", custom_updates, methods=["POST", "GET"]),
+            Route("/telegram", telegram, methods=["POST"]),
+            Route("/healthcheck", health, methods=["GET"]),
+            Route("/submitpayload", custom_updates, methods=["POST", "GET"]),
         ]
     )
-
     webserver = uvicorn.Server(
         config=uvicorn.Config(
             app=starlette_app,
-            port=80,
+            port=port,
             use_colors=False,
             host="https://boiling-ravine-21139.herokuapp.com",
         )
     )
 
-    await webserver.serve()
+    # Run application and webserver together
+    async with application:
+        await application.start()
+        await webserver.serve()
+        await application.stop()
 
 
 if __name__ == "__main__":
