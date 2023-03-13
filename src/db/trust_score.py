d4c663fc06f0b42006aff84341a422e05497ef83
Farhod <mr.hokimiyon@gmail.com>
2023-03-13 16:17:44 +0300

    t

--- src/db/trust_score.py
+++ src/db/trust_score.py
@@ -2,19 +2,19 @@ from .db_context import DBConn as db
 
 
 async def set_trust_score(user_id, customer_type, new_value):
-    with db() as db_ctx:
+    with await db() as db_ctx:
         db_ctx.execute(
             f'UPDATE users SET {customer_type}=\'{new_value}\' WHERE id=\'{user_id}\';')
 
 
 async def change_by_delta_trust_score(user_id, customer_type, delta):
-    with db() as db_ctx:
+    with await db() as db_ctx:
         db_ctx.execute(
             f'UPDATE users SET {customer_type}={customer_type}+{delta} WHERE id=\'{user_id}\';')
 
 
 async def get_trust_score(user_id, customer_type):
-    with db() as db_ctx:
+    with await db() as db_ctx:
         db_ctx.execute(
             f'SELECT {customer_type} FROM users WHERE id=\'{user_id}\';')
         return db_ctx.fetchone()
--- src/modules/courier.py
+++ src/modules/courier.py
@@ -13,6 +13,7 @@ async def courier_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
         [InlineKeyboardButton("Edit filters", callback_data=str(2))],
         [InlineKeyboardButton("Get your active offers", callback_data=str(3))],
     ]
+    context.user_data['page'] = 0
     reply_markup = InlineKeyboardMarkup(keyboard)
     await query.edit_message_text(
         text="Choose what you want to do", reply_markup=reply_markup
