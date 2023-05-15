40ee266bba1e65c52ad63ccfd28eb9a55710a732
Khaled Mohamed <Ex-o@users.noreply.github.com>
2023-05-15 17:04:01 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,95 +1,45 @@
-import { useEffect, useState } from "react";
-import {
-  Button,
-  Card,
-  Col,
-  Container,
-  Form,
-  InputGroup,
-  Row
-} from "react-bootstrap";
-import Items from "./static/Items.json";
-import blank from "./static/blank.jpg";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 
-function App() {
-  const [selections, setSelections] = useState(
-    Object.keys(Items).reduce((acc, cur) => ({ ...acc, [cur]: 0 }), {})
-  );
+import { Container, Navbar, Nav } from "react-bootstrap";
 
-  useEffect(() => {});
+import AboutUs from "./pages/AboutUs";
+import Contacts from "./pages/ContactUs";
+import Home from "./pages/Home";
+import Restaurant from "./pages/Restaurant";
 
-  const changeHandler = (key, inc) => (e) => {
-    setSelections({
-      ...selections,
-      [key]: Math.max(0, selections[key] + (inc ? 1 : -1))
-    });
-  };
+const App = () => (
+  <div className="App">
+    <Router>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="/">Hitchy!</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">
+                Restuarants
+              </Nav.Link>
+              <Nav.Link as={Link} to="/about">
+                About Us
+              </Nav.Link>
+              <Nav.Link as={Link} to="/contacts">
+                Contacts
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
 
-  const submitHandler = (e) => {
-    e.preventDefault();
-    const searchParams = new URLSearchParams(document.location.search);
-    const orderId = searchParams.get("orderId");
-    console.log(selections);
-    fetch(process.env.REACT_APP_API_URL, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        orderId,
-        selections
-      })
-    });
-  };
-
-  return (
-    <div className="App">
       <Container>
-        <Form onSubmit={submitHandler}>
-          <Row>
-            {Object.entries(Items).map(
-              ([key, {title, price, currency, img }]) => (
-                <Col xs={6} md={4} lg={3} className="mt-4">
-                  <Card
-                    className={
-                      selections[key] > 0
-                        ? "border border-2 border-success"
-                        : ""
-                    }
-                  >
-                    <Card.Img variant="top" src={img ? img : blank} />
-                    <Card.Body>
-                      <Card.Title>{title}</Card.Title>
-                      <Card.Text>{price}{currency}</Card.Text>
-                      <InputGroup className="mb-3">
-                        <Button
-                          className="btn-danger"
-                          disabled={selections[key] === 0}
-                          onClick={changeHandler(key, false)}
-                        >
-                          -
-                        </Button>
-                        <Form.Control value={selections[key]} />
-                        <Button
-                          className="btn-success"
-                          onClick={changeHandler(key, true)}
-                        >
-                          +
-                        </Button>
-                      </InputGroup>
-                    </Card.Body>
-                  </Card>
-                </Col>
-              )
-            )}
-          </Row>
-          <Button variant="primary" type="submit" className="my-4 mx-auto">
-            Submit
-          </Button>
-        </Form>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/restaurants/:name" element={<Restaurant />} />
+        </Routes>
       </Container>
-    </div>
-  );
-}
+    </Router>
+  </div>
+);
 
 export default App;
