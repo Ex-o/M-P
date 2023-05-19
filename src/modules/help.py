fb152be2972001bdc2108d65ebbaa30cb2dfa67c
Khaled Mohamed <Ex-o@users.noreply.github.com>
2023-05-19 14:18:24 +0100

    handle temp offers

--- src/modules/help.py
+++ src/modules/help.py
@@ -6,7 +6,7 @@ from src.db.utils import get_temp_order
 async def get_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /help is issued."""
 
-    offer = get_temp_order(update.message.text)
+    offer = await get_temp_order(update.message.text)
     await update.message.reply_text(offer["order_json"])
 
 async def help_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
